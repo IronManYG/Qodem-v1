98553b127a3b6854a4a952821741125e3f8b94d1
IronManYG <hussain.yg2010@gmail.com>
2021-11-29 14:46:30 +0300

    SetUp Authentication

--- app/src/main/java/com/example/qodem/MainActivity.kt
+++ app/src/main/java/com/example/qodem/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.qodem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import android.view.View
+import com.example.qodem.ui.authentication.AuthenticationActivity
+import com.firebase.ui.auth.AuthUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signOutButton = findViewById<Button>(R.id.sign_out_button)
+        // : Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        signOutButton.setOnClickListener { onClick(signOutButton) }
+    }
+
+    private fun onClick(v: View) {
+        if (v.id == R.id.sign_out_button) {
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener { // user is now signed out
+                    startActivity(Intent(this@MainActivity, AuthenticationActivity::class.java))
+                    finish()
+                }
+        }
     }
 }
--- app/src/main/java/com/example/qodem/ui/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/example/qodem/ui/authentication/AuthenticationActivity.kt
@@ -1,4 +1,97 @@
 package com.example.qodem.ui.authentication
 
-class AuthenticationActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.qodem.MainActivity
+import com.example.qodem.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthenticationActivity : AppCompatActivity(){
+
+    companion object {
+        const val TAG = "AuthenticationActivity"
+    }
+
+    // Get a reference to the ViewModel scoped to this Fragment.
+    private val viewModel by viewModels<AuthenticationViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+
+        val authButton = findViewById<Button>(R.id.auth_button)
+
+        // : Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        authButton.setOnClickListener { startSignIn() }
+
+        // : If the user was authenticated, send him to RemindersActivity
+
+        // Observe the authentication state so we can know if the user has logged in successfully.
+        // If the user has logged in successfully, send them to the RemindersActivity.
+        // If the user did not log in successfully, display an error message.
+        viewModel.authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                AuthenticationViewModel.AuthenticationState.AUTHENTICATED -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else -> Log.e(
+                    TAG,
+                    "Authentication state that doesn't require any UI change $authenticationState"
+                )
+            }
+        })
+    }
+
+    // Caller
+    private fun startSignIn() {
+        // Give users the option to sign in / register with their email or Google account. If users
+        // choose to register with their email, they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.PhoneBuilder().setDefaultNumber("sa", "538517374").build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            // ... options ...
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    // Receiver
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { result: FirebaseAuthUIAuthenticationResult? ->
+        // Handle the FirebaseAuthUIAuthenticationResult
+        // ...
+        if (result != null) {
+            onSignInResult(result)
+        }
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            Log.i(
+                TAG,
+                "Successfully signed in user " +
+                        "${FirebaseAuth.getInstance().currentUser?.displayName}!"
+            )
+        } else {
+            // Sign in failed
+            Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+        }
+    }
+
 }
