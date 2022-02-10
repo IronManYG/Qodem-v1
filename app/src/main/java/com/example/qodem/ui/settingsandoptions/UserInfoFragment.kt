a6ad03f15af04fd935fd9cbbc0d7018a1e0c4a06
IronManYG <hussain.yg2010@gmail.com>
2022-02-10 17:01:05 +0300

    Start the logic of UserInfoFragment

---
+++ app/src/main/java/com/example/qodem/ui/settingsandoptions/UserInfoFragment.kt
@@ -0,0 +1,30 @@
+package com.example.qodem.ui.settingsandoptions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.qodem.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class UserInfoFragment : Fragment() {
+
+    companion object {
+        const val TAG = "UserInfoFragment"
+    }
+
+    private val viewModel: UserInfoViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.user_info_fragment, container, false)
+    }
+
+}
