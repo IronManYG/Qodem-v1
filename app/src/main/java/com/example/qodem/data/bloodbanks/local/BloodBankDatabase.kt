ce8ac76204a693b3db10db9da375b48b915fe7bb
hussain.yg <hussain.yg2010@gmail.com>
2022-05-07 14:53:45 +0300

    Update callback logic of bloodBack dataBase

--- app/src/main/java/com/example/qodem/data/bloodbanks/local/BloodBankDatabase.kt
+++ app/src/main/java/com/example/qodem/data/bloodbanks/local/BloodBankDatabase.kt
@@ -1,10 +1,14 @@
 package com.example.qodem.data.bloodbanks.local
 
+import android.util.Log
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.qodem.data.Converters
+import com.example.qodem.data.bloodbanks.remote.BloodBankNetworkMapper
+import com.example.qodem.data.bloodbanks.remote.BloodBanksRetrofit
+import com.example.qodem.data.bloodbanks.repository.BloodBankRepository
 import com.example.qodem.di.module.ApplicationScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -17,6 +21,7 @@ abstract class BloodBankDatabase : RoomDatabase() {
 
     companion object {
         const val DATABASE_NAME: String = "bloodBank_db"
+        private const val TAG = "BloodBankDatabase"
     }
 
     abstract fun bloodBankDao(): BloodBankDao
@@ -25,16 +30,31 @@ abstract class BloodBankDatabase : RoomDatabase() {
     @Inject
     constructor(
         private val database: Provider<BloodBankDatabase>,
+        private val bloodBanksRetrofit: BloodBanksRetrofit,
+        private val bloodBankCacheMapper: BloodBankCacheMapper,
+        private val bloodBankNetworkMapper: BloodBankNetworkMapper,
         @ApplicationScope private val applicationScope: CoroutineScope
     ) : RoomDatabase.Callback() {
 
         override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
 
-            val dao = database.get().bloodBankDao()
+            val bloodBankDao = database.get().bloodBankDao()
 
             applicationScope.launch {
-                // TODO: add blood banks to data base
+                val networkBloodBanksResponse = bloodBanksRetrofit.getBloodBanks()
+                if (networkBloodBanksResponse.isSuccessful && networkBloodBanksResponse.body() != null) {
+                    val bloodBanks =
+                        bloodBankNetworkMapper.mapFromEntityList(networkBloodBanksResponse.body()!!)
+                    for (bloodBank in bloodBanks) {
+                        bloodBankDao.saveBloodBank(bloodBankCacheMapper.mapToEntity(bloodBank))
+                    }
+                    Log.d(TAG, "Blood Banks found!")
+                } else {
+                    val message = networkBloodBanksResponse.message()
+                    Log.e(TAG, message)
+                    Log.e(TAG, "Response not successful")
+                }
             }
         }
     }
